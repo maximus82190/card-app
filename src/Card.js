849f4d4801a4e5dfdd73313551eb8f2285ff77db
Max Daniels <maxelandaniels@gmail.com>
2022-08-16 10:32:11 +0300

    Create robot images. Attach names and emails

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow5">
+            <img alt='contacts directory' src={`https://robohash.org/test?200x200`}/>
+            <div>
+                <h2>Yankle Cohen</h2>
+                <p>yankle.cohen@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+// import { robots } from "./robots"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Card/>
+    <Card/>
+    <Card/>
+
+  </div>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
