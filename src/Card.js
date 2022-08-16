52dea0c33dad021782f04adf22b80b30c8bb4d82
Max Daniels <maxelandaniels@gmail.com>
2022-08-16 10:33:10 +0300

    Create ids and images

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({id, name, email}) => {
     return (
         <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow5">
-            <img alt='contacts directory' src={`https://robohash.org/test?200x200`}/>
+            <img alt='robot directory' src={`https://robohash.org/${id}?200x200`}/>
             <div>
-                <h2>Yankle Cohen</h2>
-                <p>yankle.cohen@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
---
+++ src/robots.js
@@ -0,0 +1,31 @@
+export const robots = [
+ {
+id: 0,
+name: 'Michael Jordan', 
+email: 'chb23@gmail.com'
+},  
+
+{
+id: 1,
+name: 'Michael Jackson', 
+email: 'iambad1987@gmail.com'
+}, 
+
+{
+id: 2,
+name: 'Michael Buble', 
+email: 'haventmetYou@gmail.com'
+}, 
+
+{
+id: 3,
+name: 'Michael Bluth', 
+email: 'her@gmail.com'
+}, 
+
+{
+id: 4,
+name: 'GOB', 
+email: 'magicianfreak@gmail.com'
+} 
+]
